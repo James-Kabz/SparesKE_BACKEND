15e73916b2bab53176ded825c05811e9aad09e4d
James-Kabz <kabogp@gmail.com>
2025-11-09 11:03:22 +0300

    feat:CRUD for picku-points

--- app/Modules/PickupPoint/Controllers/PickupPointController.php
+++ app/Modules/PickupPoint/Controllers/PickupPointController.php
@@ -2,6 +2,23 @@
 
 namespace App\Modules\PickupPoint\Controllers;
 
+use App\Modules\PickupPoint\Models\PickupPoint;
+use App\Modules\Vendors\Models\Vendor;
+
 class PickupPointController extends Controller
 {
+    // get vendor pickup points
+    public function vendorPickupPoints()
+    {
+        $user = auth()->user();
+        $vendor = Vendor::where('user_id', $user->id)->first();
+
+        if (!$vendor) {
+            return sendApiError('Vendor not found', 404);
+        }
+
+        $pickupPoints = $vendor->pickupPoints;
+
+        return sendApiResponse($pickupPoints, 'Vendor Pickup points retrieved successfully');
+    }
 }
--- app/Modules/PickupPoint/routes/api.php
+++ app/Modules/PickupPoint/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Modules\PickupPoint\Controllers\PickupPointController;
 
-Route::middleware(['auth:sanctum', ''])->group(function () {
+Route::middleware(['auth:sanctum', 'role:vendor'])->group(function () {
+    Route::get('vendor/pickup-points', [PickupPointController::class, 'vendorPickupPoints']);
     Route::apiResource('pickup-points', PickupPointController::class);
 });
