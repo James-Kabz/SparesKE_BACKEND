9d872230fbc4ad66ec5de90929227c4c47306834
James-Kabz <kabogp@gmail.com>
2025-11-09 20:23:25 +0300

    feat:CRUD for order

--- app/Modules/Orders/Models/Order.php
+++ app/Modules/Orders/Models/Order.php
@@ -4,6 +4,7 @@ namespace App\Modules\Orders\Models;
 
 use App\Models\User;
 use App\Modules\Parts\Models\Part;
+use App\Modules\PickupPoint\Models\PickupPoint;
 use App\Modules\Vendors\Models\Vendor;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -13,13 +14,13 @@ class Order extends Model
     use HasFactory;
 
     protected $table = 'orders';
-    
+
     protected $fillable = [
         'user_id',
         'vendor_id',
         'part_id',
         'status',
-        'pickup_point',
+        'pickup_point_id',
         'payment_method',
     ];
 
@@ -37,4 +38,10 @@ class Order extends Model
     {
         return $this->belongsTo(Part::class);
     }
+
+    public function pickupPoint()
+    {
+        return $this->belongsTo(PickupPoint::class);
+    }
+
 }
--- database/migrations/2025_11_08_204810_create_orders_table.php
+++ database/migrations/2025_11_08_204810_create_orders_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('orders', function (Blueprint $table) {
@@ -13,9 +12,20 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('vendor_id')->constrained()->onDelete('cascade');
             $table->foreignId('part_id')->constrained()->onDelete('cascade');
-            $table->enum('status', ['pending', 'confirmed', 'picked_up', 'cancelled', 'completed'])->default('pending');
-            $table->string('pickup_point')->nullable();
-            $table->enum('payment_method', ['on_pickup', 'mpesa'])->default('on_pickup');
+
+            $table->foreignId('pickup_point_id')->nullable()->constrained('pickup_points')->onDelete('set null');
+
+            $table->enum('status', [
+                'pending',
+                'confirmed',
+                'picked_up',
+                'cancelled',
+                'completed'
+            ])->default('pending');
+
+            $table->enum('payment_method', ['on_pickup', 'mpesa'])
+                ->default('on_pickup');
+
             $table->timestamps();
         });
     }
