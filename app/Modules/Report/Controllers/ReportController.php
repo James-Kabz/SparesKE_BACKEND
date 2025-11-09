b4d3fbb63a403bf6207bc22222194428adcbd6ef
James-Kabz <kabogp@gmail.com>
2025-11-09 23:10:53 +0300

    feat:CRUD for reports and also api for update report status

--- app/Modules/Report/Controllers/ReportController.php
+++ app/Modules/Report/Controllers/ReportController.php
@@ -2,6 +2,17 @@
 
 namespace App\Modules\Report\Controllers;
 
+use Illuminate\Http\Request;
+
 class ReportController extends Controller
 {
+
+    // update report status
+    public function updateStatus(Request $request, $id)
+    {
+        $item = $this->service->find($id);
+        $item->status = $request->status;
+        $item->save();
+        return sendApiResponse([$this->resourceName => $this->transform($item)], ucfirst($this->resourceName) . ' updated successfully.');
+    }
 }
--- app/Modules/Report/routes/api.php
+++ app/Modules/Report/routes/api.php
@@ -5,3 +5,11 @@ use App\Modules\Report\Controllers\ReportController;
 Route::middleware(['auth:sanctum', ''])->group(function () {
     Route::apiResource('reports', ReportController::class);
 });
+
+Route::middleware(['auth:sanctum', 'role:super-admin'])->group(function () {
+    Route::delete('reports/{id}', [ReportController::class, 'destroy']);
+});
+
+Route::middleware(['auth:sanctum', 'role:vendor'])->group(function () {
+    Route::put('reports/{id}/status', [ReportController::class, 'updateStatus']);
+});
