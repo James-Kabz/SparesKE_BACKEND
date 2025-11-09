2e5fd2239edba5bfe7bb4634b6eb0ca380696410
James-Kabz <kabogp@gmail.com>
2025-11-09 22:56:47 +0300

    feat:review CRUD and also fetch reviews for vendors

--- app/Modules/Review/Controllers/ReviewController.php
+++ app/Modules/Review/Controllers/ReviewController.php
@@ -2,6 +2,21 @@
 
 namespace App\Modules\Review\Controllers;
 
+use Illuminate\Http\Request;
+
 class ReviewController extends Controller
 {
+    // get vendor reviews
+    public function vendorIndex($id)
+    {
+        $vendor = \App\Modules\Vendors\Models\Vendor::find($id);
+
+        if (!$vendor) {
+            return sendApiError('Vendor not found', 404);
+        }
+
+        $reviews = $vendor->reviews;
+
+        return sendApiResponse($reviews, 'Vendor reviews retrieved successfully');
+    }
 }
--- app/Modules/Review/routes/api.php
+++ app/Modules/Review/routes/api.php
@@ -2,6 +2,14 @@
 
 use App\Modules\Review\Controllers\ReviewController;
 
-Route::middleware(['auth:sanctum', ''])->group(function () {
-    Route::apiResource('reviews', ReviewController::class);
+Route::middleware(['auth:sanctum', 'role:user'])->group(function () {
+    Route::apiResource('reviews', ReviewController::class)->except(['destroy']);
+});
+
+Route::middleware(['auth:sanctum', 'role:super-admin'])->group(function () {
+    Route::delete('reviews/{id}', [ReviewController::class, 'destroy']);
+});
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('vendor/{id}/reviews', [ReviewController::class, 'vendorIndex']);
 });
